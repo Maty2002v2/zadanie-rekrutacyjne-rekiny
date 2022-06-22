d481dd57080387137f980a1222a2460e41079702
maty <matikster1@wp.pl>
2022-06-22 13:02:41 +0200

    Create global styles

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@ import TheSlider from "./components/TheSlider.vue";
 
 import SliderSlide from "./types/SliderSlide";
 
+import "@/styles/globalStyles.css";
+
 export default defineComponent({
   name: "App",
   components: {
@@ -17,21 +19,21 @@ export default defineComponent({
     const FETCH_IMAGES: SliderSlide[] = [
       //lub bardziej content niż images
       {
-        path: "@/assets/1.jpg",
+        nameImage: "1.jpg",
         header: "Quisque vulputate mi consectetur",
         subheader: "Quisque vulputate mi consectetur",
         content:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere quis mi a fermentum. Nulla vel fringilla quam. Quisque a nulla euismod, finibus est eu, molestie lectus. Nam rhoncus nec nibh sit amet rhoncus. Donec mauris dolor, suscipit a felis auctor, dignissim imperdiet mi. Nulla facilisi. Praesent lobortis in purus in bibendum. Pellentesque eleifend, dui sit amet scelerisque hendrerit, mauris diam fermentum massa, et posuere nisl orci sed mi. In leo libero, sollicitudin eu felis in, egestas ultrices dui. Integer sed libero egestas, faucibus lectus ac, consectetur enim.",
       },
       {
-        path: "@/assets/2.jpg",
+        nameImage: "2.jpg",
         header: "Pellentesque eleifend, dui sit amet",
         subheader: "Mauris diam fermentum massa.",
         content:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere quis mi a fermentum. Nulla vel fringilla quam. Quisque a nulla euismod, finibus est eu, molestie lectus. Nam rhoncus nec nibh sit amet rhoncus. Donec mauris dolor, suscipit a felis auctor, dignissim imperdiet mi. Nulla facilisi. Praesent lobortis in purus in bibendum. Pellentesque eleifend, dui sit amet scelerisque hendrerit, mauris diam fermentum massa, et posuere nisl orci sed mi. In leo libero, sollicitudin eu felis in, egestas ultrices dui. Integer sed libero egestas, faucibus lectus ac, consectetur enim.",
       },
       {
-        path: "@/assets/3.jpg",
+        nameImage: "3.jpg",
         header: "Nam rhoncus nec nibh sit amet",
         subheader: "Donec mauris dolor, suscipit a felis auctor.",
         content:
